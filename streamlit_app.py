8085b4e24a41226bf163e013fa3b2f1ffe61bdf6
mhamilton94 <120267278+mhamilton94@users.noreply.github.com>
2022-12-20 11:19:57 +0000

    Update streamlit_app.py
    Used correct command for text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.body('Blueberry Oatmeal')
-streamlit.body('Kale, Spinach & Rocket Smoothie')
-streamlit.body('Scrambled Eggs')
+streamlit.text('Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Scrambled Eggs')
