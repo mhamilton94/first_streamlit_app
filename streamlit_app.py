63a4a59c8b8175dd889d7a9c0673da8746d7a4aa
mhamilton94 <120267278+mhamilton94@users.noreply.github.com>
2022-12-23 12:07:43 +0000

    Update streamlit_app.py
    Moved imports to top

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,7 +14,7 @@ streamlit.text('🥑 Avocado Toast')
 
 streamlit.header('Build Your Own Smoothie')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,7 +30,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like?', 'kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
+# import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalises JSON response
@@ -34,8 +38,10 @@ fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
 # Output as a table
 streamlit.dataframe(fruityvice_normalised)
 
-import snowflake.connector
+# Stop here for troubleshooting
+streamlit.stop
 
+# import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
